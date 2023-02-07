5865c0b470d764044e7464313d2f149fc84a3275
Susana Muñoz <su.munozh@gmail.com>
2023-02-07 11:48:16 -0300

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const nomCampeonato = document.querySelector("#nombreCampeonato");
+
+ function agregarCampeonato(){
+
+
+    document.querySelector("#nombreCampeonato").value
+}
+const campeonato = new Campeonato("13ª Campeonato los Colihues"); 
+
+   
